88e17c0c169c5d16eb795a8acb982a5a9dbb18c6
Dave Gamberg <dgamberg@gmail.com>
2015-11-15 14:06:51 -0600

    adding cart functions, delete functions

--- server/public/assets/scripts/app.js
+++ server/public/assets/scripts/app.js
@@ -1,50 +1,70 @@
-var myApp = angular.module('myApp', ['ngRoute']);
+var myApp = angular.module('myApp',  ['ngRoute']);
 
-myApp.controller('MainController', ['$scope', '$http', function($scope, $http){
-
-
-}]);
-myApp.controller('MenuController', ['$scope', '$http', function($scope, $http){
+myApp.controller('MenuController', ['$scope', '$http', 'shoppingCart' ,function($scope, $http, shoppingCart){
+    //Menu Display
     $scope.menu = {};
     $scope.menuArray = [];
-
-    //GET
     $scope.displayMenu = function(){
         $http.get('/data').then(function(response){
-            console.log(response.data)
             $scope.menuArray = response.data;
         });
     };
-    ////READ all messages
     $scope.displayMenu();
+
+    //Shopping Cart functions
+    $scope.currentCart = shoppingCart.getShoppingCart();
+    $scope.addToOrder = function(menuItem){
+        $scope.currentCart.push(menuItem);
+    }
+    $scope.removeFromOrder = function(index){
+        $scope.currentCart.splice(index, 1);
+    }
 }]);
 
+//Shopping Cart Factory
+myApp.factory('shoppingCart', function(){
+    var currentCart = [];
+    var shoppingCart = {};
+
+    shoppingCart.getShoppingCart = function(){
+        return currentCart;
+    }
+    return shoppingCart;
+});
+
+
+//Routes
 myApp.config(['$routeProvider', function($routeProvider){
    $routeProvider.
        when('/home', {
           templateUrl: "/views/home.html",
        }).
        when('/appetizers', {
-          templateUrl: "views/appetizers.html",
+          templateUrl: "views/menu/appetizers.html",
           controller: "MenuController"
        }).
        when('/burgers', {
-           templateUrl: "views/burgers.html",
+           templateUrl: "views/menu/burgers.html",
            controller: "MenuController"
        }).
        when('/salads', {
-           templateUrl: "views/salads.html",
+           templateUrl: "views/menu/salads.html",
            controller: "MenuController"
        }).
        when('/sandwiches', {
-           templateUrl: "views/sandwiches.html",
+           templateUrl: "views/menu/sandwiches.html",
            controller: "MenuController"
        }).
        when('/kids-menu', {
-           templateUrl: "views/kids.html",
+           templateUrl: "views/menu/kids.html",
+           controller: "MenuController"
+       }).
+       when('/shopping-cart', {
+           templateUrl: "views/shoppingCart.html",
            controller: "MenuController"
        }).
        otherwise({
           redirectTo: '/home'
        })
 }]);
+
