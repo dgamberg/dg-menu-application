cd8353332980fa6da85987f1288b48be9b3820e1
Dave Gamberg <dgamberg@gmail.com>
2015-11-14 18:50:34 -0600

    adding sub pages using ng-include

--- server/public/assets/scripts/app.js
+++ server/public/assets/scripts/app.js
@@ -1,59 +1,50 @@
 var myApp = angular.module('myApp', ['ngRoute']);
 
-myApp.controller('MenuDisplayController', ['$scope', '$http', function($scope, $http){
-   $scope.menu = {};
-   $scope.menuArray = [];
+myApp.controller('MainController', ['$scope', '$http', function($scope, $http){
 
-   //GET
-   $scope.displayMenu = function(){
-      $http.get('/data').then(function(response){
-         console.log(response.data)
-         $scope.menuArray = response.data;
-      });
-   };
-   ////READ all messages
-   $scope.displayMenu();
-
-   ////DELETE MESSAGE
-   //$scope.deleteMessage = function(message){
-   //   console.log(message.id);
-   //   $http.put('/data/' + message.id).then(function(){
-   //      $scope.getMessages();
-   //   });
-   //};
-
-   //POST
-   //$scope.addMenu = function(response){
-   //   $http.post('/data', response).then(function(response){
-   //      console.log(response);
-   //      $scope.menu = {};
-   //      $scope.displayMenu();
-   //   });
-   //};
 
 }]);
+myApp.controller('MenuController', ['$scope', '$http', function($scope, $http){
+    $scope.menu = {};
+    $scope.menuArray = [];
 
-//myApp.controller('AppetizerDisplayController', ['$scope', '$http', function($scope, $http){
-//      $scope.appetizers = [];
-//
-//}]);
+    //GET
+    $scope.displayMenu = function(){
+        $http.get('/data').then(function(response){
+            console.log(response.data)
+            $scope.menuArray = response.data;
+        });
+    };
+    ////READ all messages
+    $scope.displayMenu();
+}]);
 
 myApp.config(['$routeProvider', function($routeProvider){
    $routeProvider.
        when('/home', {
-          templateUrl: "/assets/views/routes/index.html",
-          controller: "MenuDisplayController"
-
+          templateUrl: "/views/home.html",
        }).
-      //when('/code', {
-      //    templateUrl: "/assets/views/routes/code.html",
-      //    controller: "SomeController"
-      //}).
        when('/appetizers', {
-          templateUrl: "/assets/views/appetizers.html",
-          controller: "MenuDisplayController"
+          templateUrl: "views/appetizers.html",
+          controller: "MenuController"
+       }).
+       when('/burgers', {
+           templateUrl: "views/burgers.html",
+           controller: "MenuController"
+       }).
+       when('/salads', {
+           templateUrl: "views/salads.html",
+           controller: "MenuController"
+       }).
+       when('/sandwiches', {
+           templateUrl: "views/sandwiches.html",
+           controller: "MenuController"
+       }).
+       when('/kids-menu', {
+           templateUrl: "views/kids.html",
+           controller: "MenuController"
        }).
        otherwise({
-          redirectTo: 'home'
+          redirectTo: '/home'
        })
 }]);
