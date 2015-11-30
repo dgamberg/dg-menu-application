59e5e484beba1dd3b880e81d691bff18ff844542
Dave Gamberg <dgamberg@gmail.com>
2015-11-30 09:21:37 -0600

    updating files and branch

--- server/app.js
+++ server/app.js
@@ -4,12 +4,14 @@ var path = require('path');
 var bodyParser = require('body-parser');
 
 var mongoose = require('mongoose');
-var passport = require('./strategies/user');
 var session = require('express-session');
+var passport = require('./strategies/user');
+
 
 var register = require('./routes/register');
 var user = require('./routes/user');
 var index = require('./routes/index');
+var customer = require('./routes/customer');
 
 // App Set //
 app.set("port", (process.env.PORT || 5000));
@@ -17,6 +19,7 @@ app.set("port", (process.env.PORT || 5000));
 // Routes
 app.use('/register', register);
 app.use('/user', user);
+app.use('/customer', customer);
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({expanded: true}));
@@ -87,29 +90,33 @@ app.get('/data/:id', function(req,res){
 });
 
 
+// Mongo Passport Connection //
+var mongoPassportURI = "mongodb://localhost:27017/user_passport_session";
+var mongoPassportDB = mongoose.connect(mongoPassportURI).connection;
+mongoPassportDB.on('error', function(err){
+    if(err) console.log("MONGO ERROR: ", err);
+});
+mongoPassportDB.once('open', function(){
+    console.log("Connected to Passport, meow!");
+});
+
+// Mongo Customer Connection //
+var mongoCustomerDBURI = "mongodb://localhost:27017/customerData";
+var mongoCustomerDB = mongoose.connect(mongoCustomerDBURI).connection;
+mongoCustomerDB.on('error', function(err){
+    if(err) console.log("MONGO ERROR: ", err);
+});
+mongoCustomerDB.once('open', function(){
+    console.log("Connected to Customer DB!, meow!");
+});
+
+app.use('/', index);
+
+//WildCard
 app.get("/*", function(req,res){
     var file = req.params[0] || "/views/index.html";
     res.sendFile(path.join(__dirname, "./public", file));
 });
-
-// Mongo Connection //
-var mongoURI = "mongodb://localhost:27017/user_passport_session";
-//var mongoURI = "";
-var mongoDB = mongoose.connect(mongoURI).connection;
-
-
-
-mongoDB.on('error', function(err){
-    if(err) console.log("MONGO ERROR: ", err);
-});
-
-mongoDB.once('open', function(){
-    console.log("Connected to Mongo, meow!");
-});
-
-
-app.use('/', index);
-
 
 // Listen //
 app.listen(app.get("port"), function(){
