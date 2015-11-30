5c9ea2157c4807320319c74d1f037fe510b0e34f
Dave Gamberg <dgamberg@gmail.com>
2015-11-30 08:19:51 -0600

    added passport

--- server/app.js
+++ server/app.js
@@ -1,16 +1,45 @@
 var express = require('express');
 var app = express();
-
 var path = require('path');
 var bodyParser = require('body-parser');
 
+var mongoose = require('mongoose');
+var passport = require('./strategies/user');
+var session = require('express-session');
+
+var register = require('./routes/register');
+var user = require('./routes/user');
+var index = require('./routes/index');
+
+// App Set //
+app.set("port", (process.env.PORT || 5000));
+
+// Routes
+app.use('/register', register);
+app.use('/user', user);
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({expanded: true}));
+
+// Passport Session Configuration //
+app.use(session({
+    secret: 'secret',
+    key: 'user',
+    resave: 'true',
+    saveUninitialized: false,
+    cookie: {maxage: 600000, secure: false}
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+// SQL PG Functions
 var pg = require('pg');
 //var connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/dg-restaurant-menu';
 var connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/d7ok4h9na8io6d';
 //var Firebase = require("firebase");
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ expanded: true }));
 
 // Get all the messages information
 app.get('/data', function(req,res){
@@ -39,7 +68,7 @@ app.get('/data', function(req,res){
 });
 
 //Get an Item by ID
-app.put('/data/:id', function(req,res){
+app.get('/data/:id', function(req,res){
     console.log(req.params.id);
     var menuItem = req.params.id;
     var result = [];
@@ -63,7 +92,26 @@ app.get("/*", function(req,res){
     res.sendFile(path.join(__dirname, "./public", file));
 });
 
-app.set("port", process.env.PORT || 5000);
+// Mongo Connection //
+var mongoURI = "mongodb://localhost:27017/user_passport_session";
+//var mongoURI = "";
+var mongoDB = mongoose.connect(mongoURI).connection;
+
+
+
+mongoDB.on('error', function(err){
+    if(err) console.log("MONGO ERROR: ", err);
+});
+
+mongoDB.once('open', function(){
+    console.log("Connected to Mongo, meow!");
+});
+
+
+app.use('/', index);
+
+
+// Listen //
 app.listen(app.get("port"), function(){
-    console.log("Listening on port: ", app.get("port"));
+    console.log("Listening on port: " + app.get("port"));
 });
